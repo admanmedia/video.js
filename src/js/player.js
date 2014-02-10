c6b672c23a18ba161e64124174575cfd59901d4e
minoru <mkatogi@gmail.com>
2014-02-10 15:34:21 +0100

    FIX syntax

--- src/js/player.js
+++ src/js/player.js
@@ -562,6 +562,7 @@ vjs.Player.prototype.techCall = function(method, arg){
       // @TODO: setPoster id undefined when used techOrder youtube
       if(method != "setPoster"){
         this.tech[method](arg);
+      }
     } catch(e) {
       vjs.log(e);
       throw e;
